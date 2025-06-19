d8a643f4964838a7165502e4d812bdc5e73d181e
Michaël Breedveld <m.breedveld@rbel.io>
2025-06-19 22:38:29 +0200

    remove old cog.toml and add script

---
+++ scripts/update_chart_yaml.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+CHART_NAME=$1
+NEW_VERSION=$2
+CHART_FILE="charts/$CHART_NAME/Chart.yaml"
+TMP_FILE="${CHART_FILE}.tmp"
+
+echo "Updating $CHART_FILE to version $NEW_VERSION"
+
+# Use POSIX-compatible sed replacement (no -i)
+sed "s/^version:.*/version: $NEW_VERSION/" "$CHART_FILE" > "$TMP_FILE" && mv "$TMP_FILE" "$CHART_FILE"
